3aa31cb7619265ca19226b7f3ec82cafeffbd879
Paul Knopf <Paul Knopf>
2016-02-16 20:26:46 -0500

    Added the idl for the interfaces that will be used.

--- src/PushSourceFilters/stdafx.cpp
+++ src/PushSourceFilters/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// PushSourceFilter.pch will be the pre-compiled header
+// PushSourceFilters.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
